088f09d2642563ed8d2e7b2f6e4fe4e222405840
Akshat7274 <akshat1205aj@gmail.com>
2023-05-26 18:16:42 +0530

    Registration Page and WTForms

---
+++ wtform_fields.py
@@ -0,0 +1,18 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, EqualTo
+
+class RegistrationForm(FlaskForm):
+    username = StringField('username_label',validators=[
+        InputRequired(message="Username can not be Blank"),
+        Length(min=4,max=25,message="Username must be between 4 and 25 characters")
+    ])
+    password = PasswordField('password_label',validators=[
+        InputRequired(message="Password can not be Blank"),
+        Length(min=8,max=25,message="Password must be between 8 and 25 characters")
+    ])
+    confirm = PasswordField('confirm_label',validators=[
+        InputRequired(message="Confirm Password can not be Blank"),
+        EqualTo('password',message="The Two Passwords Must Match")
+    ])
+    submit_button = SubmitField('Create')
