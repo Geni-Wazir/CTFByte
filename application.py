d4e0d29f69e9383a1b7bc762c73cef0794f1dd3b
Akshat7274 <akshat1205aj@gmail.com>
2023-05-26 23:49:50 +0530

    Models Integration User Table

--- application.py
+++ application.py
@@ -1,14 +1,40 @@
 from flask import Flask, render_template
 from wtform_fields import *
+from flask_sqlalchemy import SQLAlchemy
+from models import *
 
 app = Flask(__name__)
 app.secret_key = 'secret#Text'
+
+# Configuring Database
+db_user = "postgres"
+db_pass = "postgres%40123"
+db_host = "localhost"
+db_port = "5432"
+db_name = "postgres"
+conn_link = "postgresql://" + db_user + ":" + db_pass + "@" + db_host + ":" + db_port + "/" + db_name
+
+app.config['SQLALCHEMY_DATABASE_URI'] = conn_link
+db.init_app(app)
 
 @app.route("/",methods=['GET','POST'])
 def index():
     reg_form = RegistrationForm()
     if reg_form.validate_on_submit():
-        return "Great Success!"
+        username = reg_form.username.data
+        password = reg_form.password.data
+
+        with app.app_context():
+            # Check if User already exists
+            user_object = User.query.filter_by(username=username).first()
+            if user_object:
+                return "This Username is already taken by another User"
+            
+            # Add User to Database
+            user = User(username=username,password=password)
+            db.session.add(user)
+            db.session.commit()
+            return "Inserted into Database"
     return render_template("index.html", form=reg_form)
 
 if __name__ == "__main__":
